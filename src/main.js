4ec5bb9bcc6527a1ba1943455467906771828247
wowaidummy <wowaidummy@gmail.com>
2026-09-21 21:02:23 +0530

    short description of what changed

---
+++ src/main.js
@@ -0,0 +1,24 @@
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+
+document.querySelector("#app").innerHTML = "<p>loading...</p>";
+
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+  .then(response => response.json())
+  .then(data => {
+    let media;
+
+    if (data.media_type === "image") {
+      media = `<img src="${data.url}"/>`;
+    } else {
+      media = `<video src="${data.url}" controls></video>`;
+    }
+
+    document.querySelector("#app").innerHTML = `
+      <h1>${data.title}</h1>
+      ${media}
+      <p>${data.explanation}</p>
+    `;
+  })
+  .catch(err => {
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`;
+  });
